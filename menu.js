e06d99e72060a0b4947aa23f5bac24d2626d902b
Marlon Martins <80369150+snitraMnolraM@users.noreply.github.com>
2023-10-20 15:52:12 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -15,3 +15,80 @@ overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
 
+
+
+class FormSubmit{
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button)
+
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
+
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    }
+
+    getFormObject() {
+        const FormObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((fields)=>{
+            FormObject[fields.getAttribute('name')] = fields.value;
+        });
+        return FormObject;
+    }
+
+    onSubmission(event) {
+        event.preventDefault();
+        event.target.disabled = true;
+        event.target.innerText = "Enviando...";
+
+
+    }
+
+
+
+
+    async sendForm(event){
+        try{
+            this.onSubmission(event);
+            await fetch(this.url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept:"application/json",
+                },
+                body: JSON.stringify(this.getFormObject()),
+            });
+            this.displaySuccess();
+        } catch (error) {
+            this.displayError();
+            throw new Error(error);
+        }
+
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListener("click", this.sendForm);
+        return this;
+    }
+
+}
+
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!<h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.<h1>",
+});
+formSubmit.init();
+
+
